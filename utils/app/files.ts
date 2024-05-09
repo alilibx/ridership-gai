e7e5cf1970df53feec6d7320c6c0535df413b92e
Ali Alhashimi <libmacali@gmail.com>
2024-05-09 20:43:37 +0400

    update embedding and fix content issues

--- utils/app/files.ts
+++ utils/app/files.ts
@@ -17,12 +17,13 @@ export const extractContentFromJsonFile = (filePath: string, type: string, langu
     }
     const fileContent = fs.readFileSync(filePath, 'utf8');
     const jsonArray = JSON.parse(fileContent).allservices;
+    const ignoredFieldsNames = ['channels','id','parent_id','transaction_id','unique_id','related_documents','apply_now_link','most_popular','service_classification','disclaimer','updated_at','service_packages']
     const contentArray = jsonArray.map((item: any) => {
         const fields = Object.keys(item);
         // If the field is 'channels' then join the array of channel name and desciptions in to one string 
-        const content = fields
-            .filter((field) => field !== 'channels')
-            .map((field) => item[field])
+        var content = fields
+            .filter((field) => !ignoredFieldsNames.includes(field))
+            .map((field) => field + ': ' + item[field])
             .join(' ');
 
         // if the field is 'channels' then join the array of channel name and desciptions in to one string
@@ -34,7 +35,16 @@ export const extractContentFromJsonFile = (filePath: string, type: string, langu
         // Add the channels string to the content string
         content.concat(channels);
 
+        // Remove all html tags from the content string
+        content = removeHtmlTags(content);
+
         return { content, metadata: { unique_id: item.unique_id, name: item.name, type: type, language: language } };
     });
     return contentArray;
+}
+
+// Create a function to take string and removed all html tags including urls and return a string
+export const removeHtmlTags = (str: string) => {
+    const regex = /(<([^>]+)>)/ig;
+    return str.replace(regex, '');
 }
